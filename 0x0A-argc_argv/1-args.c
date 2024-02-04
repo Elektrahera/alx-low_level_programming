7c8d7cbf471d5a50957e83c92dfefacbc1dbf24e
Mercy <morramercy@gmail.com>
2024-02-04 07:13:24 -0800

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main: name the program
+ * @argc: number of arguments
+ * @argv: name
+ * Return nothing
+ */
+int main(int argc, char *argv[])
+{
+  printf("argc = %d\n", argc);
+  printf("argv[0]=%s\n",argv[0]);
+
+  return 0;
+
+}
