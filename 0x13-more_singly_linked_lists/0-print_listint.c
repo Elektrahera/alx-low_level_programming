b5703de505910fcedfbf3b2e5390155b7cf29df9
Mercy <morramercy@gmail.com>
2024-02-06 10:10:14 -0800

    updates

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - print lists
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: print lists
+ * 
+ */
+size_t print_listint(const listint_t *h);
+{
+    int n;
+    print_listint *next;
+} listint_t *h;
+
+size_t print_listint(const listint_t *h) *h, *new;
+h = 0;
+new = (print_listint *) malloc(sizeof(listint_t))
+
+  if (head == 0)
+    {
+     h = new
+    }
+  else
+    {
+     h -> next =new
+    }
+    
